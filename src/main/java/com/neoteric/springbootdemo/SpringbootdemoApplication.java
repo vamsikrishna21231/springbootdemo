45c992aea317104d89d78a3917b8f9110bdce868
VAMSI KRISHNA <vamsikrishnagaddam07@gmail.com>
2024-12-04 15:28:13 +0530

    learning core java

---
+++ src/main/java/com/neoteric/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.neoteric.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+
+}
